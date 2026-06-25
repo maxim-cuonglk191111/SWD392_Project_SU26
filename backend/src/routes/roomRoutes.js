5fb476702349eaea95aeca55267179b8ad936d7a
Nguyễn Bình An <167419245+TomOutfit@users.noreply.github.com>
2026-06-25 07:25:39 +0700

    Delete backend directory

--- backend/src/routes/roomRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const { authenticate, optionalAuth } = require('../middleware/authMiddleware');
-const { listRooms, getRoom, createRoom } = require('../controllers/roomController');
-
-router.get('/', optionalAuth, listRooms);
-router.get('/:roomId', optionalAuth, getRoom);
-router.post('/', authenticate, createRoom);
-
-module.exports = router;
